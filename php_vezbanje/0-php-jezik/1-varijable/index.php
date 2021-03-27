7375258f10f18d97cc60f3d84440db7f8b3e73dc
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-03-27 11:38:52 +0100

    php jezik, osnovno

---
+++ php_vezbanje/0-php-jezik/1-varijable/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>Varijable</h1>
+    <?php
+        $webcon_radionice = 'Kodiranje uzivo sredom, a bogami i subotom, a bogami i kad stignem u toku nedelje';
+        echo $webcon_radionice;
+
+        echo '<hr>';
+        
+        $music = 'jazz';
+        echo $music;
+
+    ?>
+</body>
+</html>
